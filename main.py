e31f4667cd9baee4787f870e523d8854f2f9cd51
HaydenHildreth <36833433+HaydenHildreth@users.noreply.github.com>
2022-04-19 14:07:05 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from tkinter import *
+import time
+
+root = Tk()
+root.title("Click Counter")
+root.columnconfigure(0, weight=1)
+count = 0
+time_var = 10.00
+flag = False
+
+
+def countdown():
+    global time_var
+    global flag
+    if time_var == 0 or time_var < 0:
+        flag = False
+    if flag:
+        time_var -= 1
+        timer.configure(text=f"Your time left is: {time_var}")
+        time.sleep(1)
+    root.after(100, countdown)
+
+
+def increase():
+    global count
+    global flag
+    global time_var
+    if not flag:
+        flag = True
+        countdown()
+    if time_var > 0:
+        count += 1
+        counter.configure(text=f"Your count is: {count}")
+
+
+counter = Label(text=f"Your count is: {count}")
+counter.grid()
+timer = Label(text=f"Your time left is: {time_var}")
+timer.grid()
+button = Button(text="Click to increase", command=increase)
+button.grid()
+
+
+root.mainloop()
